680a5dc0a1c3da9075f4d19f6987820866beb1c9
Scooby <abereg01@pm.me>
2024-09-27 07:21:14 +0200

    colors

---
+++ colors/_todo/doomchad/rosepine-moon.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# settings
+mode="dark"
+wall="fill"
+
+# colors
+bg="#e0def4"
+fg="#232136"
+bl="#393552"
+wh="#e0def4"
+r="#eb6f92"
+g="#3e8fb0"
+y="#f6c177"
+b="#9ccfd8"
+m="#c4a7e7"
+c="#ea9a97"
+br="#908caa" # Inactive
+br2="#9ccfd8" # Active
